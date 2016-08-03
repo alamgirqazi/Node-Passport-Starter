8dd0ab2447c0c5aca9289a2980a76e7625f600bf
alamgirqazi <alamgirmunirqazi@gmail.com>
2016-08-03 14:40:51 +0500

    First Commit. Local Strategy working.

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    // For Local MongoDB database
+    // change passportdb to your db name.
+
+    'url' : 'mongodb://localhost/passportdb'
+
+    // If you use MongoLab then create an app and make changes according
+    // Here is a sample of MongoLab connection
+
+    //'url' : 'mongodb://USERNAME:PASSWORD@ds1391355.mlab.com:39655/DBNAME'
+};
